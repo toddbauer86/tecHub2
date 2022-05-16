ea5fa091764b38efe0831973fcb8dbe065544fd2
toddbauer86 <98363909+toddbauer86@users.noreply.github.com>
2022-05-16 16:36:32 -0500

    minor updates

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'bigbluedog',
+  secret: 'the chicago cubs are bad',
   cookie: {
         // Session will automatically expire in 10 minutes
         expires: 10 * 60 * 1000
